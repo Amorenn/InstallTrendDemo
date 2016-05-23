8109a9e85973afc261f017b6a3e3c0d27f13c8ea
Robert <Greisemer@gmail.com>
2016-05-23 16:04:34 -0400

    installtrend
    installtrend

--- .ebextensions/installtrend.sh
+++ .ebextensions/installtrend.sh
@@ -8,5 +8,9 @@ files:
                 owner: root
                 group: root
                 content: |
-                #!/bin/bash
-                mkdir /tstdir2/
+				#!/usr/bin/env bash
+				wget https://app.deepsecurity.trendmicro.com:443/software/agent/amzn1/x86_64/ -O /tmp/agent.rpm --quiet
+				rpm -ihv /tmp/agent.rpm
+				sleep 15
+				/opt/ds_agent/dsa_control -r
+				/opt/ds_agent/dsa_control -a dsm://agents.deepsecurity.trendmicro.com:443/ "tenantID:6F8DA5CA-B30E-1F2A-B5FE-86DBFAA8E7D9" "tenantPassword:C8D84FEA-21A5-4988-2779-ADB8EE09CD31"
