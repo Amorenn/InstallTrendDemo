8e4b32931df9d7b84bc731f6afa2b5333e3d495f
Robert <Greisemer@gmail.com>
2016-05-23 13:17:30 -0400

    First Commit

---
+++ .ebextensions/installtrend.sh
@@ -0,0 +1,14 @@
+commands:
+        create_install_trend_folder:
+                command: "mkdir /opt/elasticbeanstalk/hooks/appdeploy/installtrend"
+                ignoreErrors: true
+files:
+        "/opt/elasticbeanstalk/hooks/appdeploy/installtrend/1_install_trend.sh":
+                mode: "000755"
+                owner: root
+                group: root
+                content: |
+                #!/bin/bash
+                mkdir /tstdir/
+
+
